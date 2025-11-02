4464221f3d5af251f660559f83fe05244432e3e1
Chocoeater <lonelypain98@gmail.com>
2025-11-03 00:45:54 +0300

    register user model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # Поля, отображаемые в списке пользователей
+    list_display = ('username', 'email', 'timezone', 'notification_preferences', 'is_active', 'is_staff')
+
+    # Фильтры справа
+    list_filter = ('timezone', 'notification_preferences', 'is_active', 'is_staff', 'is_superuser')
+
+    # Поля для поиска
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+
+    # Порядок сортировки
+    ordering = ('username',)
+
+    # Группировка полей при редактировании
+    fieldsets = UserAdmin.fieldsets + (
+        ("Уведомления", {
+            "fields": ("timezone", "notification_preferences")
+        }),
+    )
+
+    # Группировка полей при создании
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Уведомления", {
+            "fields": ("timezone", "notification_preferences"),
+        }),
+    )
