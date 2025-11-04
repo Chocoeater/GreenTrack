db35c4f26fb98def6ad7f90b8fd62a89f23efc68
Chocoeater <lonelypain98@gmail.com>
2025-11-04 16:23:20 +0300

    register care and plant apps in admin panel

--- plants/admin.py
+++ plants/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from plants.models import Plant
+
+
+@admin.register(Plant)
+class PlantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'species', 'user', 'created_at')
+    list_filter = ('created_at', 'species', 'user')
+    search_fields = ('name', 'species', 'user__username', 'user__email')
+    readonly_fields = ('created_at',)
+    autocomplete_fields = ['user']
