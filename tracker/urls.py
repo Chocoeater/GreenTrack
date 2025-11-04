2d34e63a9e67e24bf592bd28c763b49a8ce90844
Chocoeater <lonelypain98@gmail.com>
2025-11-04 16:22:56 +0300

    add urls for care app

--- tracker/urls.py
+++ tracker/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('plants.urls')),
+    path('api/', include('care.urls')),
 ]
