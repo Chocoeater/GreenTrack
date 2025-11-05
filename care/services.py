766bd70ca6eb3a7117f67eaf179fd8f0cb10434c
Chocoeater <lonelypain98@gmail.com>
2025-11-05 23:56:34 +0300

    add srvice func for send message

--- care/services.py
+++ care/services.py
@@ -3,10 +3,11 @@ from datetime import date, timedelta
 from rest_framework.exceptions import ValidationError
 
 from care.models import CareTask, CareTaskLog
+from users.models import User
 
 
 
-def mark_task_as_done(task: CareTask, notes: str = "") -> CareTask:
+def mark_task_as_done(task: User, notes: str = "") -> CareTask:
     """
     Отмечает задачу по уходу как выполненную на текущий день.
 
@@ -42,3 +43,36 @@ def mark_task_as_done(task: CareTask, notes: str = "") -> CareTask:
     )
 
     return task
+
+def send_daily_care(user: User, due_tasks: list[CareTask]) -> bool:
+    if not due_tasks.exists():
+        return False
+
+    subject = "Ежедневное напоминание о уходе"
+
+    plant_tasks = []
+    for task in due_tasks:
+        plant_tasks.append(f"- {task.care_type.name} для {task.plant.name}")
+
+    plant_list = "\n".join(plant_tasks)
+
+    message = (
+        f"Привет, {user.first_name or user.username}!\n\n"
+        f"Сегодня тебе нужно позаботиться о своих зеленых питомцах:\n\n"
+        f"{plant_list}\n\n"
+        f"Не забудь проверить и отметь выполнение задач по уходу!"
+    )
+
+    print('ОТПРАВКА СООБЩЕНИЯ: ' + '\n' + message)
+    # try:
+    #     send_mail(
+    #         subject=subject,
+    #         message=message,
+    #         from_email=settings.DEFAULT_FROM_EMAIL,
+    #         recipient_list=[user.email],
+    #         fail_silently=False,
+    #     )
+    #     return True
+    # except Exception as e:
+    #     print(f"Ошибка отправки сводного напоминания: {e}")
+    #     return False
