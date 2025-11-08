1048115ba4e01780d9c034c88f4267ccbf6be43a
Chocoeater <lonelypain98@gmail.com>
2025-11-08 22:51:12 +0300

    add func to create path to save img and add her in model Plant

--- plants/models.py
+++ plants/models.py
@@ -3,6 +3,19 @@ from django.db import models
 
 User = get_user_model()
 
+def plant_image_upload_to(instance, filename):
+    """
+    Динамически генерирует путь для загрузки изображения растения.
+
+    Args:
+        instance (Plant): Экземпляр модели Plant
+        filename (str): Оригинальное имя файла
+
+    Returns:
+        str: Путь вида 'images/plants/user_1/photo.jpg'
+    """
+    return f'images/plants/user_{instance.user.id}/{filename}'
+
 class Plant(models.Model):
     """
     Модель для представления растения в системе.
@@ -19,7 +32,7 @@ class Plant(models.Model):
 
     name = models.CharField(max_length=200, verbose_name='Имя растения', help_text='Введите имя растения')
     species = models.CharField(max_length=200, verbose_name='Вид растения', help_text='Введите вид растения')
-    image = models.ImageField(upload_to='images/plants/user_{user_id}/', verbose_name='Изображение растения', help_text='Загрузите изображение растения', null=True, blank=True)
+    image = models.ImageField(upload_to=plant_image_upload_to, verbose_name='Изображение растения', help_text='Загрузите изображение растения', null=True, blank=True)
     notes = models.TextField(verbose_name='Заметки', help_text='Введите заметки о растении', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='plants')
